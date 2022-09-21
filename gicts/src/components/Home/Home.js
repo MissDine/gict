a0ca94b4b33488a651adc8f055a9fab5a86ac310
MissDine <enidnyatichi1212@gmail.com>
2022-09-21 20:34:03 +0300

    Feature edit

---
+++ gicts/src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from "../Navbar/Navbar"
+import Header from "../Header/Header"
+import Footer from "../Footer/Footer"
+
+function Home() {
+  return (
+    <div className='col-sm-12'>
+        <Navbar/>
+        <Header/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
